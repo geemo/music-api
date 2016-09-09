2f2f01dc5ceb3765b9c9caa6512b20d9172e45a9
geemo <153330685@qq.com>
2016-09-09 21:35:53 +0800

    完善封装

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 'use strict';
 
+/* 网易云音乐常用api封装
+ * @author: geemo<153330685@qq.com>
+ * @version: 1.0.0
+ * @create at: 2016-09-09
+ * @license: MIT
+ */
+
 const encryptData = require('./utils/crypto.js').encryptData;
 const request = require('./utils/request.js');
 
 class MusicApi {
 	constructor() {}
 
-	search(s, limit, offset, type) {
-		limit = limit || 30;
+	/* 通过关键字搜索音乐信息
+	 * @param {String} keyword 曲名，专辑名或歌手名等
+	 * @param {Number} limit 返回的歌曲条数，默认10条
+	 * @param {Number} offset 跳过的歌曲条数, 主要是为了分页
+	 * @param {Number} type
+	 * @return {Promise}
+	 */
+	search(keyword, limit, offset, type) {
+		limit = limit || 10;
 		offset = offset || 0;
 		type = type || 1;
 
 		const url = 'http://music.163.com/weapi/cloudsearch/get/web?csrf_token=';
 		const data = {
-			s: s,
+			s: keyword,
 			type: type,
 			limit: limit,
 			total: true,
@@ -21,13 +35,33 @@ class MusicApi {
 			csrf_token: ''
 		};
 
-		return this
-				._request(url, data, 'json')
-				.then(resData => {
-					
-				}, err => Promise.reject(err));
+		return this._request(url, data, 'json')
+			.then(resData => {
+				if (resData.code === 200) {
+					resData.result.songs = resData.result.songs.map(song => {
+						return {
+							name: song.name,
+							id: song.id,
+							authors: song.ar,
+							album: {
+								id: song.al.id,
+								name: song.al.name,
+								picUrl: song.al.picUrl
+							},
+							duration: song.dt,
+							mv: song.mv
+						};
+					});
+				}
+
+				return resData;
+			}, err => err);
 	}
 
+	/* 通过id搜索歌曲详细信息
+	 * @param {String|Array} songIds 歌曲id
+	 * @return {Promise}
+	 */
 	detali(songIds) {
 		const url = 'http://music.163.com/weapi/v1/song/detail';
 		const data = {
@@ -35,22 +69,60 @@ class MusicApi {
 			csrf_token: ''
 		};
 
-		return this._request(url, data);
+		return this._request(url, data, 'json')
+			.then(resData => {
+				if (resData.code === 200) {
+					resData.songs = resData.songs.map(song => {
+						return {
+							name: song.name,
+							id: song.id,
+							authors: song.ar,
+							album: {
+								id: song.al.id,
+								name: song.al.name,
+								picUrl: song.al.picUrl
+							},
+							duration: song.dt,
+							mv: song.mv
+						};
+					});
+				}
+
+				return resData;
+			}, err => err);
 	}
 
-	mp3Url(songIds, br) {
-		br = br || 320000;
-
+	/* 批量搜索歌曲url
+	 * @param {String|Array} songIds 歌曲id
+	 * @return {Promise}
+	 */ 
+	mp3Url(songIds) {
 		const url = 'http://music.163.com/weapi/song/enhance/player/url?csrf_token=';
 		const data = {
 			ids: Array.isArray(songIds) ? songIds : [songIds],
-			br: br,
+			br: 320000,
 			csrf_token: ''
 		};
 
-		return this._request(url, data);
+		return this._request(url, data, 'json')
+			.then(resData => {
+				if (resData.code === 200) {
+					resData.data = resData.data.map(d => {
+						return {
+							id: d.id,
+							url: d.url
+						};
+					});
+				}
+
+				return resData;
+			}, err => err);
 	}
 
+	/* 根据id过去歌词，包括原文歌词和翻译成中文的歌词
+	 * @param {String} songId 歌曲id
+	 * @return {Promise}
+	 */ 
 	lyric(songId) {
 		const url = 'http://music.163.com/weapi/song/lyric?csrf_token=';
 		const data = {
@@ -62,9 +134,50 @@ class MusicApi {
 			csrf_token: ''
 		};
 
-		return this._request(url, data);
+		return this._request(url, data, 'json')
+			.then(resData => {
+				if(resData.code === 200 && !resData.nolyric) {
+					return {
+						lyric: resData.lrc.lyric,
+						tlyric: resData.tlyric.lyric,
+						code: resData.code
+					};
+				}
+
+				return resData;
+			}, err => err);
 	}
 
+	/* 通过id获取mv
+	 * @param {String} mvId
+	 * @return {Promise}
+	 */ 
+	mv(mvId) {
+		const url = 'http://music.163.com/weapi/mv/detail/';
+		const data = {
+			id: mvId,
+			csrf_token: ''
+		};
+
+		return this._request(url, data, 'json')
+			.then(resData => {
+				if(resData.code === 200) {
+					return {
+						coverUrl: resData.data.cover,
+						duration: resData.data.duration,
+						brs: resData.data.brs,
+						code: resData.code
+					};
+				}
+
+				return resData;
+			}, err => err);
+	}
+
+	/* 获取歌单信息
+	 * @param {String} playListId 歌单id
+	 * @return {Promise}
+	 */ 
 	playList(playListId) {
 		const url = 'http://music.163.com/weapi/v3/playlist/detail?csrf_token=';
 		const data = {
@@ -73,19 +186,15 @@ class MusicApi {
 			csrf_token: ''
 		};
 
-		return this._request(url, data);
+		return this._request(url, data, 'json');
 	}
 
-	mv(mvId) {
-		const url = 'http://music.163.com/weapi/mv/detail/';
-		const data = {
-			id: mvId,
-			csrf_token: ''
-		};
-
-		return this._request(url, data);
-	}
-
+	/* 发送http请求
+	 * @param {String} url 请求的目的url
+	 * @param {String|Object} data 发送的数据,可以为字符串或json对象
+	 * @param {String} dataType 对响应数据进行何种格式(json|plain text)的数据解析
+	 * @return {Promise}
+	 */ 
 	_request(url, data, dataType) {
 		dataType = dataType || 'text';
 
@@ -98,11 +207,11 @@ class MusicApi {
 				method: 'POST',
 				form: encryptData(data)
 			}, (err, res, result) => {
-				if(err) return reject(err);
-				if(dataType === 'json') {
+				if (err) return reject(err);
+				if (dataType === 'json') {
 					try {
 						result = JSON.parse(result);
-					} catch(e) {
+					} catch (e) {
 						return reject(e);
 					}
 
--- test/api_test.js
+++ test/api_test.js
@@ -4,8 +4,38 @@ const MusicApi = require('../index.js');
 
 const musicApi = new MusicApi();
 
+// musicApi
+//     .search('color 3d', 1)
+//     .then(result => {
+//         console.log(JSON.stringify(result, null, ' '));
+//     }, console.error.bind(console));
+
 musicApi
-    .search('color 3d')
+    .mp3Url('34380473')
     .then(result => {
-        console.log(result.result.songs);
+        console.log(JSON.stringify(result, null, ' '));
     }, console.error.bind(console));
+
+// musicApi
+//     .mv('5360424')
+//     .then(result => {
+//         console.log(JSON.stringify(result, null, ' '));
+//     }, console.error.bind(console));
+
+// musicApi
+//     .detali('34380473')
+//     .then(result => {
+//         console.log(JSON.stringify(result, null, ' '));
+//     }, console.error.bind(console));
+
+// musicApi
+//     .lyric('399367218')
+//     .then(result => {
+//         console.log(JSON.stringify(result, null, ' '));
+//     }, console.error.bind(console));
+
+// musicApi
+//     .playList('379372244')
+//     .then(result => {
+//         console.log(JSON.stringify(result, null, ' '));
+//     }, console.error.bind(console));
